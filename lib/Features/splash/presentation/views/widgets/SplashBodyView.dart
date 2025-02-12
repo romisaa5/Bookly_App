b66faedb03851b4e54e38402bfca910b7bc5bbca
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-02-12 17:07:16 +0200

    update splash

--- lib/Features/splash/presentation/views/widgets/SplashBodyView.dart
+++ lib/Features/splash/presentation/views/widgets/SplashBodyView.dart
@@ -7,9 +7,12 @@ class SplashBodyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: 3,
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.Logo)],
+      children: [Image.asset(AssetsData.Logo)
+      ,
+      Text('Read Free Books',textAlign: TextAlign.center,)],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
-import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -13,8 +12,8 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: KprimaryColor
+      theme: ThemeData.dark().copyWith(
+     
       ),
       debugShowCheckedModeBanner: false,
       home: SplashView(),
