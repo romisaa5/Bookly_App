dab4abc4fcc0481fd5c6b17b3981829eab22bd35
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-02-12 16:01:26 +0200

    create splash_view_body

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -9,6 +11,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return GetMaterialApp(
+      home: SplashView(),
+    );
   }
 }
