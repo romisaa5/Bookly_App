b85f221dee4b9cd36b73f4c3aa28d0dc03a9fcb1
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-02-14 23:32:29 +0200

    add custom appbar

---
+++ lib/Features/home/presentation/views/widgets/custom_appbar.dart
@@ -0,0 +1,21 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0,vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.Logo,width: 120,height: 20,),
+        Icon( FontAwesomeIcons.magnifyingGlass,size: 24,)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/Features/home/presentation/views/widgets/custom_appbar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomAppbar(
+          
+        )
+      ],
+    );
+  }
+}
