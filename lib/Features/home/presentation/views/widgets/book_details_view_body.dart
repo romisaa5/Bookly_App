5db4183e876465c9ff656f9b1d84c0f457db821f
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-02-16 04:36:45 +0200

    add SimilarBooksListview

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart
 import 'package:bookly/Features/home/presentation/views/widgets/custom_bookdetails_appbar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_box_action.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/similar_books_listview.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,7 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           CustomBookdetailsAppbar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: widthscreen * .2),
+            padding: EdgeInsets.symmetric(horizontal: widthscreen * .22),
             child: CustomBookImage(),
           ),
           SizedBox(height: 34,),
@@ -38,7 +39,15 @@ class BookDetailsViewBody extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: CustomBoxAction(),
-            )
+            ),SizedBox(height: 40,),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('You can also like',style:styles.textstyle14.copyWith(
+                fontWeight: FontWeight.w600
+              ) ,),
+            ),
+            SizedBox(height: 20,),
+            SimilarBooksListview()
         ],
       ),
     );
