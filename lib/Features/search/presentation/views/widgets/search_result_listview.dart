e60e6928c20f03e760414740ea9dfc6067e743dc
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-02-19 18:47:33 +0200

    create book model

--- lib/Features/search/presentation/views/widgets/search_result_listview.dart
+++ lib/Features/search/presentation/views/widgets/search_result_listview.dart
@@ -7,7 +7,7 @@ class SearchResultListview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      physics: BouncingScrollPhysics(),
+     
       padding: EdgeInsets.zero,
       itemCount: 10,
       itemBuilder: (context, index) {
